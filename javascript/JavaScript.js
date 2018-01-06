904684556a83afaab4a834e69dd159a04ba43e8d
DavidLoMain <david.lo.main@gmail.com>
2018-01-05 19:42:20 -0800

    Added Contact Me
    - Have not written the javascript to actually save what is submitted in the box yet
    - Based off of the Guess Number game created while taking the Crash Course on Javascript

---
+++ javascript/JavaScript.js
@@ -0,0 +1,34 @@
+
+
+// Will use in future for changeable background color
+// Learned from Punchcode crash course
+//  document.body.style.background = `rgb(${red}, ${green}, ${blue})`
+
+function changeColor(){
+  
+  /*
+  Lucky Colors Background Color Buttons
+  
+  
+  
+  Gold
+  var red = 255
+  var green = 215
+  var blue = 0
+  
+  Yellow
+  var red = 255
+  var green = 255
+  var blue = 0
+  
+  Gray
+  var red =
+  var green =
+  var blue = 
+  */
+  
+  
+  
+ document.body.style.background = `rgb(${red}, ${green}, ${blue})`
+  
+}
